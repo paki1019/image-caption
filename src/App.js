b7da4ff86be9db77d95a9ffe634fb12737e4d836
jinholee <jinholee@nts-corp.com>
2020-06-07 16:53:21 +0900

    이미지 캡쳐

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from "react";
+import html2canvas from "html2canvas";
 
-function App() {
+const App = () => {
+  const [downloadUrl, setDownloadUrl] = useState();
+
+  const onClickHandler = useCallback(() => {
+    html2canvas(document.querySelector("#capture")).then((canvas) => {
+      console.log(canvas);
+      setDownloadUrl(canvas.toDataURL("image/jpg"));
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        id="capture"
+        style={{ padding: "10px", background: "#f5da55" }}
+        onClick={onClickHandler}
+      >
+        <h4 style={{ position: "relative", color: "#000" }}>Hello world!</h4>
+        <h2 style={{ position: "absolute", top: "30px" }}>문자열</h2>
+      </div>
+      <a href={downloadUrl} download={"download.jpg"}>
+        다운로드
+      </a>
     </div>
   );
-}
+};
 
 export default App;
