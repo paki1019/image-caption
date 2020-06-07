f22a05626ad67df0041ff9ae608cc5a621b5b809
jinholee <jinholee@nts-corp.com>
2020-06-07 18:58:38 +0900

    Updates

---
+++ precache-manifest.0a3a06c7cea43cb719600b8c70266654.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53fbad93086c8bba878ab4ee2bd45dc4",
+    "url": "/image-caption/index.html"
+  },
+  {
+    "revision": "dd65701af0f4b234ddd2",
+    "url": "/image-caption/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1d45611cb7ecb925bf0b",
+    "url": "/image-caption/static/js/2.21aa1de8.chunk.js"
+  },
+  {
+    "revision": "e2d595485404c29bf7636215726700fd",
+    "url": "/image-caption/static/js/2.21aa1de8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd65701af0f4b234ddd2",
+    "url": "/image-caption/static/js/main.6a0dceb2.chunk.js"
+  },
+  {
+    "revision": "9ca7cdf4e9ed1a4bf04c",
+    "url": "/image-caption/static/js/runtime-main.4ea2e904.js"
+  }
+]);
